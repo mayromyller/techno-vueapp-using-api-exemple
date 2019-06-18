1720ddeaaf1860726797b1e5b3d2a7460f2a5705
Mayro <deehmyller11@gmail.com>
2019-06-17 23:09:24 -0300

    🧢 add style in header

--- techo-cursos/src/App.vue
+++ techo-cursos/src/App.vue
@@ -1,31 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header/>
+    <router-view></router-view>
   </div>
 </template>
+<script>
+import Header from "@/components/Header.vue";
 
+export default {
+  components: {
+    Header
+  }
+};
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap");
+body {
+  margin: 0px;
+  font-family: "Josefin Sans", sans-serif;
+  color: #234;
 }
-#nav {
-  padding: 30px;
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
 }
-
-#nav a {
+a {
   font-weight: bold;
-  color: #2c3e50;
+  text-decoration: none;
+  color: #234;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+a.router-link-active {
+  color: #4b8;
 }
 </style>
--- techo-cursos/src/router.js
+++ techo-cursos/src/router.js
@@ -1,25 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Contato from './views/Contato.vue'
+import Cursos from './views/Cursos.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/contato',
+      name: 'contato',
+      component: Contato
+    },
+    {
+      path: '/cursos',
+      name: 'cursos',
+      component: Cursos
     }
   ]
 })
