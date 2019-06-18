2edf9da5940f1b0c2bd1f754b40e75a55c4b3c1a
Mayro <deehmyller11@gmail.com>
2019-06-18 08:24:00 -0300

    🤕 add view curso in cursos and styles

--- techo-cursos/src/views/Contato.vue
+++ techo-cursos/src/views/Contato.vue
@@ -3,10 +3,26 @@
     <div v-if="loading">
       <Loading/>
     </div>
-    <div v-if="api">
-      <h1>Contato</h1>
-      <p>{{api}}</p>
-    </div>
+    <transition>
+      <div v-if="api">
+        <h1>{{api.titulo}}</h1>
+        <p>{{api.descricao}}</p>
+        <ul>
+          <li>
+            <b>Email</b>
+            {{api.contato.email}}
+          </li>
+          <li>
+            <b>Telefone</b>
+            {{api.contato.telefone}}
+          </li>
+          <li>
+            <b>Endereço</b>
+            {{api.contato.endereco}}
+          </li>
+        </ul>
+      </div>
+    </transition>
   </div>
 </template>
 
